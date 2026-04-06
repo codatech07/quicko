d6a1ca527917dd264772fedd788acca07b3bd3fe
ahmad <fox.gaming998@gmail.com>
2026-04-06 20:10:55 +0300

    resend mail

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,15 +1,10 @@
-const nodemailer = require("nodemailer");
+const { Resend } = require("resend");
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
 const sendEmail = async (options) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS, // App Password
-    },
-  });
-  await transporter.verify();
- return transporter.sendMail({
-    from: `Quicko App <${process.env.EMAIL_USER}>`,
+  await resend.emails.send({
+    from: process.env.EMAIL_FROM,
     to: options.email,
     subject: options.subject,
     text: options.message,
