691d75c2a975e9100068bee1fe44fb049e0466af
ahmad <fox.gaming998@gmail.com>
2026-04-18 20:12:45 +0300

    delete image shop

--- controllers/shopController.js
+++ controllers/shopController.js
@@ -149,6 +149,7 @@ exports.updateShop = asyncHandler(async (req, res) => {
 
   shop.images = imagesArray;
 }
+console.log("FILES:", req.files.length);
   await shop.save();
   return successResponse(res, "Shop updated successfully", shop);
 });
